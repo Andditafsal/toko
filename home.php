c61d53b06dcaf113fdb223792f29d7ac848d2b04
Nur-Fajri-Maulidia <79305134+Nur-Fajri-Maulidia@users.noreply.github.com>
2021-05-27 22:06:16 +0700

    Ajax-landing
    Menambahkan file home.php, koneksi.php, style.css

---
+++ home.php
@@ -0,0 +1,110 @@
+<?php
+session_start();
+if (!issert($_sessionis['is_login'])){
+    header('location:login.php');
+}
+
+?>
+<head>
+<title>toko Bunga</title>
+<link rel="stylesheet" href="style.css">
+</head>
+
+
+<div class='Navbar'>
+<ul>
+<li><a href=''>Home</a></li>
+<li><a href='catalog/index.php'>Catalog</a></li>
+<li><a href=''>About</a></li>
+<li><a href=''>Contct</a></li>
+<ii><a href="lougout.php" class="">lougout</a> </a></ii>
+
+</ul>
+
+</div>
+<center>
+    <main role="main" class="container">
+
+    <div>
+        <h1>Selamat Datang <?php echo $_SESSION[?></h1>
+    </div>
+
+    </main>
+</center>
+        
+<div class='foto'>
+        <img src=''>
+        <h1>Buket</h1>
+        <p>Harga Rp. 50.000</p>
+        <a href='bunga/index.php'>Beli Sekarang</a>
+    </div><div class='gambar'>
+    
+    <div class='foto'>
+        <img src=''>
+        <h1>Buket</h1>
+        <p>Harga Rp. 50.000</p>
+        <a href='bunga/index.php'>Beli Sekarang</a>
+    </div>
+        
+    <div class='foto'>
+        <img src=''>
+        <h1>Buket</h1>
+        <p>Harga Rp. 50.000</p>
+        <a href='bunga/index.php'>Beli Sekarang</a>
+    </div>
+
+    <div class='foto'>
+        <img src=''>
+        <h1>Buket</h1>
+        <p>Harga Rp. 50.000</p>
+        <a href='bunga/index.php'>Beli Sekarang</a>
+    </div>
+
+    <div class='foto'>
+        <img src=''>
+        <h1>Buket</h1>
+        <p>Harga Rp. 50.000</p>
+        <a href='bunga/index.php'>Beli Sekarang</a>
+    </div>
+
+    <div class='foto'>
+        <img src=''>
+        <h1>Buket</h1>
+        <p>Harga Rp. 50.000</p>
+        <a href='bunga/index.php'>Beli Sekarang</a>
+    </div>
+
+    <div class='foto'>
+        <img src=''>
+        <h1>Buket</h1>
+        <p>Harga Rp. 50.000</p>
+        <a href='bunga/index.php'>Beli Sekarang</a>
+    </div>
+
+    <div class='foto'>
+        <img src=''>
+        <h1>Buket</h1>
+        <p>Harga Rp. 50.000</p>
+        <a href='bunga/index.php'>Beli Sekarang</a>
+    </div>
+
+    <div class='foto'>
+        <img src=''>
+        <h1>Buket</h1>
+        <p>Harga Rp. 50.000</p>
+        <a href='bunga/index.php'>Beli Sekarang</a>
+    </div>
+
+    <div class='foto'>
+        <img src=''>
+        <h1>Buket</h1>
+        <p>Harga Rp. 50.000</p>
+        <a href='bunga/index.php'>Beli Sekarang</a>
+    </div>
+    
+    <div class='foto'></div>
+     <p> Copyright 2021 - <a href=''>Toko Bunga</a></p>
+    </div>
+    </body>
+    </html>
+

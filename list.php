e4219e60d63273a130f17f5ad3e7e033982bbfaa
andita.fs <79301323+Andditafsal@users.noreply.github.com>
2021-06-15 07:55:09 +0700

    Add files via upload

--- list.php
+++ list.php
@@ -1,43 +1,66 @@
-<?php
-    include "koneksi.php";
-    $sql = "SELECT * FROM ol_bunga";
-$result = mysqli_query($cnn, $sql);
-
-mysqli_close($cnn);
-?>
-<div>
-    <a href="pendaftaran.php">Pendaftaran</a>
-    <table border="1">
-        <thead>
+<section width="150%">
+  
+    <table border="1" cellpadding="10" cellspacing="0" width="115%" height="60%">
+        <thead style="background-color:#cf31ff">
             <tr>
-                <th>no_order</th>
-                <th>tgl_order</th>
-                <th>nama_pemesan</th>
-                <th>alamat</th>
-                <th>telp</th>
-                <th>Opsi</th>
+                <th>No.</th>
+                <th>nama_bunga</th>
+                <th>Gambar</th>
+                <th>Harga</th>
+                <th>Aksi</th>
             </tr>
         </thead>
-        <body>
-        <?php
-            while($row = mysqli_fetch_assoc($result)) {
-            
-                ?>
-            <tr>
-                <td><?php echo $row["no_order"]?></td>
-                <td><?php echo $row["tgl_order"]?></td>
-                <td><?php echo $row["nama_pemesan"]?></td>
-                <td><?php echo $row["alamat"]?></td>
-                <td><?php echo $row["telp"]?></td>
+        <tbody>
+            <?php
+            $nomor = 0;
+            $sql = "SELECT * FROM bunga ORDER BY id_bunga ASC";
+            $result = $con_object->query($sql);
 
-                <td>
-                    <a href="edit.php?id=<?php echo $row["no_order"]?>">Edit</a>
-                    <a href="#">Hapus</a>
-                </td>
-            </tr>
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    ?>
+                    <tr>
+                        <td><?php echo ++$nomor; ?>.</td>
+                        <td><?php echo $row['nama']; ?></td>
+                        <td>
+                            <img src="pages/produk/gambar/<?php echo $row['gambar']; ?>" width="100" alt="">
+                        </td>
+                        <td><?php echo $row['harga'] ?></td>
+                        <td>
+                            <a href="?page=edit-data&id_bunga=<?php echo $row['id_bunga']; ?>"><button style="background-color:yellow; border-radius:25px; color:black">Edit</button></a> &bull;
+                            <form style="display: inline;" method="POST">
+                                <input type="hidden" name="id_bunga" value="<?php echo $row['id_bunga']; ?>">
+                                <button style="background-color:red; border-radius:25px; color:white" onclick="return confirm('Yakin ? Anda Ingin Menghapus Data Ini ?')" type="submit" name="btn-hapus">
+                                    Hapus
+                                </button>
+                            </form>
+                        </td>
+                     
+                    </tr>
             <?php
                 }
+            } else {
+                echo "<tr><td colspan='6'><b><i>Note : Data Tidak Ada</i></b></td></tr>";
+            }
             ?>
-        </body>
+        </tbody>
     </table>
-</div>
+        </section>
+
+<?php
+if (isset($_POST['btn-hapus'])) {
+    $id_bunga = $_POST['id_bunga'];
+
+    $sql = "DELETE FROM bunga WHERE id_bunga = $id_bunga";
+
+    if ($con_object->query($sql) === TRUE) {
+        echo "<script>alert('Data Berhasil di Hapus');</script>";
+        echo "<script>window.location.replace('?page=produk');</script>";
+        exit;
+    } else {
+        echo "<script>alert('Data Gagal di Hapus');</script>";
+        echo "<script>window.location.replace('?page=produk');</script>";
+        exit;
+    }
+}
+?>

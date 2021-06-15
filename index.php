6be27fb9d7d09d156630eca6d2c03768ac51f211
Nur-Fajri-Maulidia <79305134+Nur-Fajri-Maulidia@users.noreply.github.com>
2021-06-15 07:59:23 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,105 +1,105 @@
 <?php
-include '../connection/koneksi.php';
-
-if (!isset($_SESSION['login'])) {
-    echo "<script>window.location.href = '../login.php'</script>";
-    return;
-}
-
-if ($_SESSION['role'] != 1) {
-    echo "<script>window.location.href = '../index.php'</script>";
-}
+require_once 'connection/koneksi.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <title>Toko bunga</title>
+    <link rel='stylesheet' href="css/index.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
 </head>
 
 <body>
 
-    <input type="checkbox" id="check">
-    <!-- header area start -->
-    <header>
-        <label for="check">
-            <i class="fas fa-bars" id="sidebar_btn"></i>
-        </label> 
-        <div class="left_area">
-            <h3>Toko<span>Bunga</span></h3>
-        </div>
-        <div class="right_area">
-            <a href="../logout.php" class="logout_btn">Logout</a>
-        </div>
-    </header>
-    <!-- header area end -->
-    <!-- Sidebar start -->
-    <div class="sidebar" style="background: color #ea00ea;">
-        <center>
-            <img src="gambar/admin.png" class="profile_admin" alt="">
-            <h4>Admin</h4>
-        </center>
-        <a href="?page=dashboard"><i class="fas fa-desktop"></i> <span>Home</span></a>
-        <a href="?page=kategori"><i class="far fa-comment"></i> <span>Tambah data</span></a>
-        <a href="?page=produk"><i class="fas fa-newspaper"></i> <span>Tabel Bunga</span></a>
-       
+    <div class='header'>
+        <h1 class="logo">Toko <span>Bunga</span> </h1>
+        <ul class="navbar">
+            <li><a href='index.php'>Home</a></li>
+            <li><a href=''>katagori</a></li>
+            <li><a href=''>Keranjang</a></li>
+            <li>
+                <?php
+                if (isset($_SESSION['login'])) { ?>
+                    <a href="logout.php"><?= $_SESSION['nama']; ?></a>
+                <?php
+                } else {
+                ?>
+                    <a href="login.php">Login</a>
+                <?php
+                }
+                ?>
+            </li>
+        </ul>
     </div>
-    <!-- Sidebar end -->    
-    <div class="content">
-        <div class="isi">
-            <div class="row">
-                <div class="col-md-2">
-                </div>
-                <div class="col-md-10">
-                    <?php
-                    if (isset($_GET['page'])) {
 
-                        switch ($_GET['page']) {
-                            case 'dashboard':
-                                include_once 'pages/dashboard.php';
-                                break;
+    <div class="gambar">
 
-                            case 'produk':
-                                include_once 'pages/produk/list.php';
-                                break;
+        <?php
+        $sql = $con_object->query("SELECT * FROM bunga");
+        $rows = $sql->num_rows;
+        if ($rows > 0) {
+            while ($data = mysqli_fetch_assoc($sql)) { ?>
 
-                            case 'tambah-produk':
-                                include_once 'pages/produk/tambah.php';
-                                break;
-
-                            case 'edit-data':
-                                include_once 'pages/produk/edit_data.php';
-                                break;
-
-                            case 'kategori':
-                                include_once 'pages/kategori.php';
-                                break;
-
-                            case 'tambah-kategori':
-                                include_once 'pages/tambah-kategori.php';
-                                break;
-
-                            case 'edit-kategori':
-                                include_once 'pages/edit-kategori.php';
-                                break;
-
-                            default:
-                                echo "404 Not Found.";
-                                break;
-                        }
-                    } else {
-                        include_once 'pages/dashboard.php';
-                    }
-                    ?>
-
+                <div class='foto'>
+                    <div style="width: 100%;height: 200px;background-image: url('<?php echo 'admin/pages/produk/gambar/' . $data['gambar']; ?>'); background-repeat: no-repeat;background-attachment: contain;background-position: center;background-size: contain;"></div>
+                    <h1><?php echo $data['nama']; ?></h1>
+                    <p>Harga <?php echo $data['harga']; ?></p>
+                    <a href="beli.php?id_bunga=<?= $data['id_bunga']; ?>"> Beli Sekarang  </a>
                 </div>
 
-            </div>
+        <?php
+            }
+        }
+        ?>
+
+</div>
+        
+    <footer class="footer-distributed">
+
+      <div class="footer-left">
+
+        <h3>Home Bunga</h3>
+        
+      </div>
+
+      <div class="footer-center">
+
+      <div>
+          
+          <p><a href="mailto:tokoBunga@gmail.com">TokoBunga@gmail.com</a></p>
+        </div>
+
+        <div>
+          
+        <p>Telp. (0876) 45256</p>
+        </div>
+
+        <div>
+          
+        <p><span>Ds.Lohbener Blok Jembatan Merah Rt/Rw 23/05 No.25 Indramayu</p>
+        </div>
+
+      </div>
+
+      <div class="footer-right">
+
+        <p class="footer-company-about">
+          <span>About</span>
+        Hub.Kami
+        </p>
+
+        <div class="footer-icons">
+
+          <a href="#"><i class="fab fa-whatsapp"></i></a>
+          <a href="#"><i class="fab fa-facebook"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          
+
+        </div>
+
+      </div>
+
+    </footer>
 </body>
 
 </html>

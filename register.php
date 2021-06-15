452813da58b3a9236c4bb80b605d47222443e496
Nur-Fajri-Maulidia <79305134+Nur-Fajri-Maulidia@users.noreply.github.com>
2021-06-15 08:02:20 +0700

    Add files via upload

--- register.php
+++ register.php
@@ -1,41 +1,101 @@
+<?php
+require_once 'connection/koneksi.php';
+
+if (isset($_SESSION['login'])) {
+    if ($_SESSION['role'] == 1) {
+        echo "<script>window.location.href = 'admin/index.php'</script>";
+        return;
+    }
+    echo "<script>window.location.href = 'index.php'</script>";
+}
+?>
 <!DOCTYPE html>
-<html>
-    <head>
-        <link rel="stylesheet" href="regist.css">
-        <title>Registrasi </title>
-    </head>
-    <body>
-        <div class="container">
-            <form method="post" action="regist_action.php">
-                <center>
-                    <h2>Regist </h2>
-                </center>
-                <div class="form-group">
-                    <label>Nama:</label>
-                    <input type="text" class="kolom" name="nama" placeholder="Masukan Nama">
-                </div>
-                <div class="form-group">
-                    <label>Username:</label>
-                    <input type="text" class="kolom" name="username" placeholder="Masukan Username">
-                </div>
-                <div class="form-group">
-                    <label>Email</label>
-                    <input type="email" class="kolom" name="email" placeholder="Masukan email">
-                </div>
-                <div class="form-group">
-                    <label>Alamat</label>
-                    <input type="alamat" class="kolom" name="alamat" placeholder="Masukan alamat">
-                </div>
-                <div class="form-group">
-                    <label>Password:</label>
-                    <input type="password" class="kolom" name="password" placeholder="Masukan Password">
-                </div>
-                <div class="form-group">
-                    <button type="submit"> Regist</button>
-                    <button type="button"><a href="login.php">Kembali</a></button>
-                </div>
-            </form>
-        </div>
-    </body>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <style>
+    .container label {
+        text-align: left;
+        color: black;
+    }
+    .container form input {
+        width: 100%;
+        height: 40px;
+        padding: 5px 0;
+        border: none;
+        background-color: #752bea;
+        font-size: 18px;
+        color:  #fafafa ;
+        border-radius: 20px;
+        text-align: left;
+    }
+</style>
+</head>
+
+<body>
+    <div></div>
+    <div class="container">
+        <h1>  Registasi </h1>
+
+
+                <form action="" method="POST">
+                        <label for="#nama">Nama Lengkap</label>
+                        <input type="text" name="nama" id="nama" required><br>
+
+                        <label for="#username">Username</label>
+                        <input type="text" name="username" id="username" required><br>
+
+         <label for="#password">Password</label>
+            <input type="password" name="password" id="password" required><br>
+
+            <label for="#confirm">Confirm Password</label>
+            <input type="password" name="confirm_password" id="confirm" required><br>
+            <button name="btn-register" value="REGISTER">register</button>
+            <!-- <input type="submit" name="btn-register" class="tombol_login" value="REGISTER"> -->
+            <p>Sudah punya akun? <a href="login.php">Login disini</a></p>
+        </form>
+    </div>
+
+    <?php
+
+    if (isset($_POST['btn-register'])) {
+
+        $nama = $_POST['nama'];
+        $username = $_POST['username'];
+        $password = mysqli_real_escape_string($con_object, $_POST['password']);
+        $confirm_password = $_POST['confirm_password'];
+
+        $result = $con_object->query("SELECT * FROM user WHERE username = '$username' ");
+
+        if (mysqli_fetch_assoc($result) > 0) {
+            echo "<script>alert('Username Sudah Terdaftar');</script>";
+            return;
+        }
+
+        if ($password !== $confirm_password) {
+            echo "<script>alert('Konfirmasi Password Tidak Sesuai');</script>";
+            return;
+        }
+
+        $password = password_hash($password, PASSWORD_DEFAULT);
+        $query = $con_object->query("INSERT INTO user (username,password,nama,role) VALUES('$username', '$password','$nama',0)");
+
+        if ($query) {
+            echo "<script>alert('Berhasil');</script>";
+            echo "<script>window.location.replace('login.php');</script>";
+        } else {
+            echo "<script>alert('Gagal');</script>";
+            echo "<script>window.location.replace('register.php');</script>";
+        }
+    }
+
+    ?>
+
+</body>
 
 </html>
